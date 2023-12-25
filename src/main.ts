1a6818c6da791a9102cd99f29f5a2c04c37c02d9
Archit Bhonsle <abhonsle2000@gmail.com>
2023-12-25 00:03:28 -0500

    barely working

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,33 @@
 import * as THREE from 'three';
 
-let SIZE = 5;
+let SIZE = 20;
+// function setSize(newSize: number) {
+//     SIZE = newSize;
+//     adjustThreeStuff();
+// }
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, SIZE * 4);
 camera.position.set(SIZE, SIZE, SIZE);
 camera.lookAt(0, 0, 0);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-function adjustThreeStuff() {
-    camera.far = SIZE * 4;
-    camera.position.setLength(Math.sqrt(3) * SIZE);
-    camera.updateProjectionMatrix();
-}
+// function adjustThreeStuff() {
+//     camera.far = SIZE * 4;
+//     camera.position.setLength(Math.sqrt(3) * SIZE);
+//     camera.updateProjectionMatrix();
+// }
 
-let state: boolean[][][] = [];
-
-function randomizeState() {
-    const new_state = [];
-    for (let i = 0; i < SIZE; ++i) {
-        const a = [];
-        for (let j = 0; j < SIZE; ++j) {
-            const b = [];
-            for (let k = 0; k < SIZE; ++k) {
-                b.push(Math.random() < 0.5);
-            }
-            a.push(b);
-        }
-        new_state.push(a);
-    }
-    state = new_state;
-}
-randomizeState();
-
+let state: boolean[][][] = Array(SIZE)
+    .fill(0)
+    .map(_ => Array(SIZE)
+        .fill(0)
+        .map(_ => Array(SIZE)
+            .fill(0)
+            .map(_ => Math.random() < 0.3)
+        )
+    );
 
 function addCube(x: number, y: number, z: number) {
     const HALF_SIZE = SIZE / 2;
@@ -56,14 +50,68 @@ function addCubes() {
     }
 }
 
-function update() {
-    SIZE = Math.floor(Math.random() * 20) + 1;
-    adjustThreeStuff();
-    randomizeState();
+function repaint() {
     scene.clear();
     addCubes();
-    renderer.render(scene, camera);
 }
 
-setInterval(update, 1000);
+const b = 6;
+function birth(x: number) {
+    return x == b;
+}
+const s = [5, 6, 7];
+function survives(x: number) {
+    return s.includes(x);
+}
+
+function step() {
+    const newState = Array(SIZE)
+        .fill(0)
+        .map(_ => Array(SIZE)
+            .fill(0)
+            .map(_ => Array(SIZE)
+                .fill(0)
+                .map(_ => false)
+            )
+        );
+    for (let i = 0; i < SIZE; ++i) {
+        for (let j = 0; j < SIZE; ++j) {
+            for (let k = 0; k < SIZE; ++k) {
+                let now = state[i][j][k];
+
+                let count = 0;
+                for (let x = Math.max(0, i - 1); x <= Math.min(SIZE - 1, i + 1); ++x) {
+                    for (let y = Math.max(0, j - 1); y <= Math.min(SIZE - 1, j + 1); ++y) {
+                        for (let z = Math.max(0, k - 1); z <= Math.min(SIZE - 1, k + 1); ++z) {
+                            if (state[x][y][z]) count += 1;
+                        }
+                    }
+                }
+
+                if (now && !survives(count)) now = false;
+                else if (!now && birth(count)) now = true;
+
+                newState[i][j][k] = now;
+            }
+        }
+    }
+    return newState;
+}
+
+function update() {
+    // setSize(Math.floor(Math.random() * 20) + 1);
+    repaint();
+    state = step();
+}
+
+let lastTime = 0;
+function animate(time: number) {
+    if (time - lastTime > 200) {
+        lastTime = time;
+        update();
+    }
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate)
 
